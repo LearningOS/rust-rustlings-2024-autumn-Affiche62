1ffa43409cb81e0dc8846f8410bb05d95728bf5e
Affiche <3225746653@qq.com>
2024-10-11 22:46:02 +0800

    done traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -7,13 +7,18 @@
 // No boiler plate code this time, you can do this!
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
+    
 }
-
+impl AppendBar for Vec<String>{
+    fn append_bar(self) -> Self {
+        let mut n:Vec<String> = self;
+        n.push(String::from("Foo"));
+        n.push(String::from("Bar"));
+        n
+    }
+}
 // TODO: Implement trait `AppendBar` for a vector of strings.
 
 #[cfg(test)]
